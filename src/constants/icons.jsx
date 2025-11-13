d9c1ac3aeb2ed8a5a8cef9bc5954b87d586d04db
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-11-13 05:10:49 -0300

    feat(icons): Instala dependencia react-icons y crea archivo de íconos centralizado
    - Instala dependencia react-icons
    - Crea carpeta constants con archivo icons.jsx

---
+++ src/constants/icons.jsx
@@ -0,0 +1,14 @@
+import { FiMenu, FiX } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
+import { FiMapPin, FiMail, FiPhone } from "react-icons/fi";
+
+
+export const ICONS = {
+  openMenu: <FiMenu />,
+  closeMenu: <FiX />,
+  darkMode: <FiMoon />,
+  lightMode: <FiSun />,
+  email: <FiMail />,
+  phone: <FiPhone />,
+  location: <FiMapPin />,
+};
