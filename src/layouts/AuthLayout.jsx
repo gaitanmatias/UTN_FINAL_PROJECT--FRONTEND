9e219b0d6dc776b09a394f6d4b5f39aaddcc1727
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-11-13 05:33:10 -0300

    feat(Layouts): añade componente Footer a AuthLayout, MainLayout y ProfileLayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/Footer/Footer";
 
 export default function AuthLayout() {
   return (
@@ -9,6 +10,7 @@ export default function AuthLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer/>
     </>
   );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/Footer/Footer";
 
 export default function MainLayout() {
   return (
@@ -9,6 +10,7 @@ export default function MainLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer/>
     </>
   );
 }
